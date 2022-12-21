9d9805ceefa5f27ec67e83a4dac6d0a1c640d060
청하밍 <120182202+CheongHaMing@users.noreply.github.com>
2022-12-21 23:18:50 +0900

    Create CubeHistoryDTO.cs

---
+++ DTO/CubeHistoryDTO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MaplestoryOpenAPI
+{
+    [Serializable]
+    public struct CubeHistoryDTO
+    {
+        [JsonInclude] public string id;
+        [JsonInclude] public string character_name;
+        [JsonInclude] public string create_date;
+        [JsonInclude] public string cube_type;
+        [JsonInclude] public string item_upgrade_result;
+        [JsonInclude] public string miracle_time_flag;
+        [JsonInclude] public string item_equip_part;
+        [JsonInclude] public int item_level;
+        [JsonInclude] public string target_item;
+        [JsonInclude] public string potential_option_grade;
+        [JsonInclude] public string additional_potential_option_grade;
+        [JsonInclude] public List<CubeResultOptionDTO> before_potential_options;
+        [JsonInclude] public List<CubeResultOptionDTO> before_additional_potential_options;
+        [JsonInclude] public List<CubeResultOptionDTO> after_potential_options;
+        public List<CubeResultOptionDTO> after_additional_potential_options;
+
+        // [JsonPropertyName("target_item")] 과 같은 식으로 애트리뷰트 지정 가능.
+    }
+}
